2dbd7606ef4faff9bb73cc9da548d22998f94201
Kayla Lebron <ka022247@ucf.edu>
2020-02-12 12:24:50 -0500

    #95

--- demo/demo.js
+++ demo/demo.js
@@ -1,50 +1,15 @@
-class Pet {
-    constructor(props) {
-        this.props = props;
+class Cat {
+    speak = () => {
+      return ''
     }
-    getName() {
-        return this.props.name;
-    }
-    getSpecies() {
-        return this.props.species;
-    }
-    getSpeech() {
-        return "I make this sound";
-    }
-    getIdentity = () => {
-        console.log(this);
-    }
-    getSum = (a,b) => a + b
-}
 
-class Cat extends Pet {
-    constructor(props) {
-        super(props);
-        this.props.species = "cat";
+    render () {
+      return 'String'
     }
-    getSpeech() {
-        return super.getSpeech() + "Meow!!";
-    }
-}
-class Dog extends Pet {
-    constructor(props) {
-        super(props);
-        this.props.species = "dog";
-    }
-    getSpeech() {
-        return super.getSpeech() + "Ruff Ruff!!";
+
+    newMethod () {
+
     }
 }
-function sayName(pet) {
-    console.log(`My name is ${pet.getName()}, and I am a ${pet.getSpecies()}`);
-    console.log(`${pet.getSpeech()}`);
-}
-var myPet = new Cat({name:"Luna"});
-sayName(myPet);
-myPet.getIdentity();
-var myIdentity = myPet.getIdentity;
-myIdentity();
-var myDog = new Dog({name:"Fido"});
-sayName(myDog);
-myDog.getIdentity();
-console.log(myDog.getSum());
+const Luna = new Cat()
+Luna.speak()
