e25f2b6a01687036cfecf769ef458bad396a562f
Kayla Lebron <ka022247@ucf.edu>
2020-02-17 11:10:41 -0500

    merge

--- ice/w7a/MyMath.test.js
+++ ice/w7a/MyMath.test.js
@@ -4,6 +4,10 @@ test('adds 1 + 2 to equal 3', () => {
   expect(Sum(1, 2)).toBe(3)
 })
 
+test('produces the sum of 10 + 20, which should be 30', () => {
+  expect(Sum(10, 20)).toBe(30)
+})
+
 // ICE 
 
 // Examples for Null
