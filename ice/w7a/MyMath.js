a97abef1b9d72005d4e160b3a40fb08bad618f92
Kayla Lebron <ka022247@ucf.edu>
2020-02-17 12:26:21 -0500

    Jest

--- ice/w7a/MyMath.js
+++ ice/w7a/MyMath.js
@@ -1,10 +1,47 @@
-export function Sum (a, b) {
-
-  return a + b
+function Sum (a, b) {
+  let result = undefined;
+  if (typeof a == 'number' && typeof b == 'number') {
+    result = a + b
+  } 
+  return result
 }
 
-console.log(Sum(2,3)==5);
-console.log(Sum(-10,-20)==-30);
-console.log((Sum(-10,"Twenty")));
+function AddList (array) {
+  let result = undefined
+  if (array.length > 0){
+    result = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (typeof array[i] != 'number'){
+        result = undefined;
+        break;
+     }
+     result = result + array[i];
+    }
+  }
+  return result
+}
 
-export { sum };
+function DivideBy (x, y) {
+  let result = undefined
+  if ((y != 0) && (typeof x == 'number' && typeof y == 'number')){
+    result = x / y
+  }
+  return result
+}
+
+function ContainsString (str, substr) {
+  let result = false
+  if (typeof str == 'string' && typeof substr == 'string'){
+    if (str.includes(substr))
+    result = true
+  }
+  return result
+}
+
+function ReSortedNumbers (array) {
+  
+}
+
+//console.log(Sum(-10,0));
+
+export { Sum, AddList, DivideBy, ContainsString, ReSortedNumbers };
